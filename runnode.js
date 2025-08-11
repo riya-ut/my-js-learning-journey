98c019365e6e68344c82c1f2b2e48d99466f84d4
Riya UT <156648774+riya-ut@users.noreply.github.com>
2025-08-11 10:35:37 +0530

    Add files via upload

---
+++ runnode.js
@@ -0,0 +1,1 @@
+console.log("Hello, world! first code in node");
---
+++ variables.js
@@ -0,0 +1,22 @@
+{
+var a = 12
+console.log(`The value of first a is` + " " + a)
+
+a = 125
+}
+console.log(`The value of second a is` + " " + a)
+
+{
+let b = 13
+console.log(`The First value of b is` + " " + b)
+b = 139
+// }
+// console.log(`The Second value of b is` + " " + b)
+
+// {
+const c = 14
+console.log(`The first value of c is` + " " + c)
+
+c = 140
+}
+console.log(`The second value of c is` + " " + c)
