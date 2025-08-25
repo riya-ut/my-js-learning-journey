e5b67b876d4a5fe66a913296d8ba457913dea6c2
Riya UT <156648774+riya-ut@users.noreply.github.com>
2025-08-25 22:55:07 +0530

    Add files via upload

---
+++ DataTypes.js
@@ -0,0 +1,65 @@
+//Number
+let v1 = 5
+console.log(v1)
+console.log(typeof(v1))
+
+let v2 = 5555
+console.log(v2)
+console.log(typeof(v2))
+
+let v3 = 5.8
+console.log(v3)
+console.log(typeof(v3))
+
+//String
+let str="Tap"
+console.log(str)
+console.log(typeof(str))
+let str1 = 'Academy'
+console.log(str1)
+console.log(typeof(str1))
+
+let str2 = "a"
+console.log(str2)
+console.log(typeof(str2))
+
+//boolean
+let bool1 = true
+console.log(bool1)
+console.log(typeof(bool1))
+
+//Trival
+//null
+let n = null
+console.log(n)
+console.log(typeof(n))
+
+//undefined
+let u
+console.log(u)
+console.log(typeof(u))
+
+//non primitive datatype
+
+let ar = [1,2,3,4.5]
+console.log(ar)
+console.log(typeof(ar))
+
+//array creation method 2
+let ar1 = []
+ar1[0] = "A"
+ar1[1] = "B"
+console.log(ar1)
+console.log(typeof(ar1))
+
+//objects
+let information = {
+        "firstName":"Riya",
+        "lastName" : "UT",
+        "age":25
+ }
+ console.log(information)
+ console.log(typeof(information))
+ console.log(information.lastName)
+
+ 
