ff9673ee07b862a36708bb32ffab8f1a5083e21e
Riya UT <156648774+riya-ut@users.noreply.github.com>
2025-08-26 07:54:56 +0530

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,44 @@
+//Arithemetic operators
+let v1 = 5
+let v2 = 5
+console.log(v1+v2)
+
+//sub
+let v3 = 5
+let v4 = 5
+console.log(v3-v4)
+
+//exponent
+let v5 = 5
+let v6 = 2
+console.log(v5**v6)
+
+//increment
+let v7 = 5
+v7++
+console.log(v7)
+
+//decrement
+let v8 = 5
+v8--
+console.log(v8)
+
+//Assignment operator
+
+let x= 5
+console.log(x+=x)
+
+//comparison operator
+//equal to
+let a = 5
+let b = 5
+console.log(a==b)
+
+//not equal to
+let a1 = 5
+let b1 = '5'
+console.log(a!=b)
+
+let a2 = 5
+let b2 = '5'
+console.log(a===b)  ///check the type, more accurate
