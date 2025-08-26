d94b7c8e361f82e37294d10ce9279719330514f1
Riya UT <156648774+riya-ut@users.noreply.github.com>
2025-08-26 17:02:07 +0530

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,69 @@
+//Arithemetic operators
+let v1 = 5
+let v2 = 5
+console.log(v1+v2)
+
+//sub
+let v3 = 5
+let v4 = 5
+console.log(v3-v4)
+
+//exponent
+let v5 = 5
+let v6 = 2
+console.log(v5**v6)
+
+//increment
+let v7 = 5
+v7++
+console.log(v7)
+
+//decrement
+let v8 = 5
+v8--
+console.log(v8)
+
+//Assignment operator
+
+let x= 5
+console.log(x+=x)
+
+//comparison operator
+//equal to
+let a = 5
+let b = 5
+console.log(a==b)
+
+//not equal to
+let a1 = 5
+let b1 = '5'
+console.log(a!=b)
+
+let a2 = 5
+let b2 = '5'
+console.log(a===b)  ///check the type, more accurate
+
+//String Operators
+
+var str1 = "Java "
+var str2 = "Script"
+var result = str1+str2
+console.log(result)
+
+//Logical operator
+let a3 = 111
+let b3 = 112
+console.log((a3>b3) && (a3==b3))
+
+//or operator
+let a4 = 111
+let b4 = 112
+console.log((a4<b4) || (a4==b4))
+
+let a5 = 111
+let b5 = 112
+console.log(!(a5<b5))
+
+//Ternary operator
+let c = a5>b5?a5:b5
+console.log(c)
