680e51b0b2bd5bb79c9c90c6f9e24fc8091cc9d0
Riya UT <156648774+riya-ut@users.noreply.github.com>
2025-09-02 07:20:49 +0530

    Add files via upload

---
+++ function5.js
@@ -0,0 +1,5 @@
+var str1 = prompt("Enter the first string")
+var str2 = prompt("Enter the second string")
+console.log(str1+ str2)     //result getting in browser
+alert(str1+ str2)           //result in the browser pop up(in alert box:only ok button is there)   
+confirm(str1+ str2)           //result in the browser pop up(ok and cancel button is there)   
