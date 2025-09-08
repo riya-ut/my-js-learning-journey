48e38ae510d63816e693f997482049358a849117
Riya UT <156648774+riya-ut@users.noreply.github.com>
2025-09-08 22:25:34 +0530

    Add files via upload

---
+++ arraySort7.js
@@ -0,0 +1,9 @@
+var ar = [1,2,9,3,4,5,6]
+
+var ar1 = ["a", "b", "c","d"]
+console.log(ar)
+console.log(ar.sort())      //sort
+console.log(ar.reverse())     //to reverese
+console.log(ar.concat(ar1))     //to concat
+var ar2 = ar.slice(2,5,1)                 //to slice
+console.log(ar2)
